4bb5d324f15fa4078c8a744e2b9cf68657356657
moustafa-abas <124290544+moustafa-abas@users.noreply.github.com>
2025-03-12 17:45:31 +0200

    build landing section

---
+++ src/app/components/Landing.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+import me from "../assets/IMG-20240117-WA0090.webp";
+const Landing = () => {
+  return (
+    <div className="flex  justify-between  capitalize items-center">
+      <main className="w-1/2 ">
+        <hgroup>
+          <h1 className="gradient sm:text-8xl    text-xl ">
+            front-end developer
+          </h1>
+          <p className="sm:mt-4 mt-3 sm:text-2xl">
+            hello, my name is <br />
+            <span className="gradient sm:text-4xl text-xl">
+              mostafa abas
+            </span>
+          </p>
+        </hgroup>
+        <button className=" background rounded-3xl  sm:w-40 sm:h-12 sm:mt-8 w-32 h-8 mt-3 sm:text-xl ">
+          <a href="" className=" ">
+            view my work
+          </a>
+          {/* <a href="https://drive.google.com/file/d/1BZcYJlja-osY5M6z2gItiezAz013dh9c/view?usp=sharing" className='w-full' download='mostafa-abas-cv.pdf' target="_blank"> download cv</a> */}
+        </button>
+      </main>
+      <aside className="w-1/2 ">
+        <Image src={me} alt="my photo "  />
+      </aside>
+    </div>
+  );
+};
+
+export default Landing;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', 
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,5 +14,11 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [    function ({ addUtilities }) {
+    addUtilities({
+      '.bg-clip-text': {
+        'background-clip': 'text',
+      },
+    });
+  },],
 };
